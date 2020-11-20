77320f08e977b18c633b39a40ccb32371c94acd1
Bedatin <59573904+Bedatin@users.noreply.github.com>
2020-11-20 21:34:35 +0100

    15/11/2020 Suben dias Bajan bien SE ORDENAN Sigue petando por los tiempos Notificaciones OK, SI JODER OK!!!!!!

---
+++ app/src/main/java/com/example/comidapp/notifications/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.comidapp.notifications
+
+class Constants {
+
+    companion object{
+        const val  BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY =  "AAAA7EzxoYM:APA91bEr4S56O_Owd98Mz1NWDQiUkva7W_XaIsBEBAr90GmxIRGPx_sBLgvLFpHlpgmxnqafrD3e9Agm50fS6em26cVX--VseXjQqhSP7YPaILyBcbzKd6FIPSqFk2TkGZOFHqS2kMia"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
---
+++ app/src/main/java/com/example/comidapp/notifications/NotificationData.kt
@@ -0,0 +1,6 @@
+package com.example.comidapp.notifications
+
+data class NotificationData (
+    val title: String,
+    val message: String
+)
